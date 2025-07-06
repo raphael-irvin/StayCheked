20248d13879205a5cdbf95f2bcbc65a3b6eb9a7e
Raphael Jean Irvin <raphael.jeanirvin@gmail.com>
2025-07-06 19:44:47 +0800

    Refactor AccommodationController to filter verifiable accommodations for display in the table

--- src/main/java/com/example/staycheked/controller/AccommodationController.java
+++ src/main/java/com/example/staycheked/controller/AccommodationController.java
@@ -5,6 +5,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
+import java.util.HashMap;
+
 import com.example.staycheked.model.DataStore;
 import com.example.staycheked.model.user.Accommodation;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -21,14 +24,22 @@ public class AccommodationController {
     @FXML
     private TableColumn<Accommodation, String> addressColumn;
 
+    HashMap<String, Accommodation> verifiableAccommodations = new HashMap<>();
+
     @FXML
     public void initialize() {
+        for (Accommodation accommodation : DataStore.accommodations.values()) {
+            if (accommodation.getStatus() == true) {
+                verifiableAccommodations.put(accommodation.getUserID(), accommodation);
+            }
+        }
+
         accommodationNameColumn.setCellValueFactory(new PropertyValueFactory<>("userID"));
         emailColumn.setCellValueFactory(new PropertyValueFactory<>("emailAddress"));
         phoneColumn.setCellValueFactory(new PropertyValueFactory<>("contactNo"));
         addressColumn.setCellValueFactory(new PropertyValueFactory<>("location"));
 
-        ObservableList<Accommodation> accommodations = FXCollections.observableArrayList(DataStore.accommodations.values());
+        ObservableList<Accommodation> accommodations = FXCollections.observableArrayList(verifiableAccommodations.values());
         accommodationTable.setItems(accommodations);
     }
 }
